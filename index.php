36a49d9d5781cae407f478a87f778e8e98a08f41
Minh Huy <huytm@zigexn.vn>
2025-04-02 17:13:17 +0700

    Form validation

--- index.php
+++ index.php
@@ -1,20 +1,19 @@
 <?php
-  session_start();
-  // store scalar value
-	$_SESSION['user'] = 'admin';
-	// store an array
-	$_SESSION['roles'] = ['administrator', 'approver', 'editor'];
+  require __DIR__ . '/inc/header.php';
 
+  $errors = [];
+  $inputs = [];
+
+  $request_method = strtoupper($_SERVER['REQUEST_METHOD']);
+
+  if($request_method === 'GET') {
+    require __DIR__ . '/inc/get.php';
+  } elseif ($request_method === 'POST') {
+    require __DIR__ . '/inc/post.php';
+    if ($errors > 0) {
+      require __DIR__ . '/inc/get.php';
+    }
+  }
+
+  require __DIR__ . '/inc/footer.php';
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>PHP Session Demo</title>
-</head>
-<body>
-  <a href="profile.php">Go to profile page</a>
-</body>
-</html>
