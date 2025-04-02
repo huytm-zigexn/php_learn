182099ddf78bf7430a792835be5a063a6421f252
Minh Huy <huytm@zigexn.vn>
2025-04-02 09:52:53 +0700

    variable scopes

--- index.php
+++ index.php
@@ -1,3 +1,49 @@
+<!--
+  4 variables:
+    Local
+    Global
+    Static
+    Function params
+-->
+
 <?php
-    echo "Hello";
-?>
+  $message = "hi"; //global var
+
+  function say() {
+    $message = "hello"; //local var
+    echo $message . "<br>";
+  }
+
+  echo $message . "<br>";
+  say();
+
+  function global_say() {
+    global $message; //global var
+    echo $message . "<br>";
+  }
+
+  global_say();
+
+
+  function counter() {
+    static $count = 1; //static var
+    return $count++;
+  }
+
+  echo counter() . "<br>";
+  echo counter() . "<br>";
+  echo counter() . "<br>";
+
+
+
+  function sum($numbers) { //$numbers are function params
+    $total = 0;
+    foreach($numbers as $num) {
+      $total += $num;
+    }
+    return $total;
+  }
+
+  echo sum([10, 20, 30]);
+
+?> 
