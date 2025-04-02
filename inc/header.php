a0c65fe49551ca46bae042bb9b0afdf900d8194d
Minh Huy <huytm@zigexn.vn>
2025-04-02 15:21:27 +0700

    using __DIR__ when including a file

---
+++ inc/header.php
@@ -0,0 +1,9 @@
+<?php require __DIR__.'/../config/app.php' ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo APP_NAME ?></title>
+</head>
+<body>
