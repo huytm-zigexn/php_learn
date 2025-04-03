7ed24d0c4da4e4b45e6c34773aad29d60b932f7b
Minh Huy <huytm@zigexn.vn>
2025-04-03 14:31:57 +0700

    Upload 1 file

--- index.php
+++ index.php
@@ -1,82 +1,29 @@
 <?php
 session_start();
-
-const MIN_DONATION = 1;
-
-$errors = [];
-$inputs = [];
-$valid = false;
-
-$request_method = strtoupper($_SERVER['REQUEST_METHOD']);
-
-if ($request_method === 'POST') {
-
-    // sanitize & validate amount
-    $amount = filter_input(INPUT_POST, 'amount', FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
-    $inputs['amount'] = $amount;
-    if ($amount !== false && $amount !== null) {
-        $amount = filter_var($amount, FILTER_VALIDATE_FLOAT, ['options' => ['min_range' => MIN_DONATION]]);
-        if ($amount === false) {
-            $errors['amount'] = 'The minimum donation is $1';
-        } else {
-            $valid = true;
-        }
-    } else {
-        $errors['amount'] = 'Please enter a donation amount';
-    }
-
-    // add variables to session
-    $_SESSION['valid'] = $valid;
-    $_SESSION['errors'] = $errors;
-    $_SESSION['inputs'] = $inputs;
-
-    header('Location: index.php', true, 303);
-    exit;
-} elseif ($request_method === 'GET') {
-    if (isset($_SESSION['valid'])) {
-        // get the valid state from the session
-        $valid = $_SESSION['valid'];
-        unset($_SESSION['valid']);
-    }
-
-    if (isset($_SESSION['errors'])) {
-        $errors = $_SESSION['errors'];
-        unset($_SESSION['errors']);
-    }
-
-    if (isset($_SESSION['inputs'])) {
-        $inputs = $_SESSION['inputs'];
-        unset($_SESSION['inputs']);
-    }
-}
+require_once __DIR__ . '/inc/flash.php';
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <title>PHP - without PRG</title>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="stylesheet" href="https://www.phptutorial.net/app/css/style.css"/>
+    <title>PHP File Upload</title>
 </head>
-
 <body>
-    <main>
-        <form action="index.php" method="post">
-            <h1>Donation</h1>
-            <?php if ($valid) : ?>
-                <div class="alert alert-success">
-                    Thank you for your donation of $<?= $inputs['amount'] ?? '' ?>
-                </div>
-            <?php endif ?>
-            <div>
-                <label for="amount">Amount:</label>
-                <input type="text" name="amount" value="<?= $inputs['amount'] ?? '' ?>" id="amount" placeholder="Minimum donation $<?= MIN_DONATION ?>">
-                <small><?= $errors['amount'] ??  '' ?></small>
-            </div>
-            <button type="submit">Donate</button>
-        </form>
-    </main>
+
+<?php flash('upload') ?>
+
+<main>
+    <form enctype="multipart/form-data" action="upload.php" method="post">
+        <div>
+            <label for="file">Select a file:</label>
+            <input type="file" id="file" name="file"/>
+        </div>
+        <div>
+            <button type="submit">Upload</button>
+        </div>
+    </form>
+</main>
 </body>
 </html>
