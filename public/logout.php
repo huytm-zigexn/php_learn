9dc1b8a540f206cf4ae8f8c5f7bcfb62d9446b29
Minh Huy <huytm@zigexn.vn>
2025-04-04 10:25:21 +0700

    learn about object, class, , access modifiers, constructor and destructor

--- public/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-function logout(): void
-{
-    if (is_user_logged_in()) {
-        unset($_SESSION['username'], $_SESSION['user_id']);
-        session_destroy();
-        redirect_to('login.php');
-    }
-}
