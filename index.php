1cd45bd2b93059e35fa6f5e48ea5e19ac7fcd6a9
Minh Huy <huytm@zigexn.vn>
2025-04-04 10:51:00 +0700

    typed properties and readonly properties

--- index.php
+++ index.php
@@ -1,31 +1,80 @@
 <?php
 
-class File
+class BankAccount
 {
-	private $handle;
+    public float $balance = 0;
 
-	private $filename;
-
-	public function __construct($filename, $mode = 'r')
-	{
-		$this->filename = $filename;
-		$this->handle = fopen($filename, $mode);
-	}
-
-	public function __destruct()
-	{
-		// close the filehandle
-		if ($this->handle) {
-			fclose($this->handle);
-		}
-	}
-
-	public function read()
-	{
-		// read the file contents
-		return fread($this->handle, filesize($this->filename));
-	}
+    public function __construct(float $balance)
+    {
+        $this->balance = $balance;
+    }
 }
 
-$f = new File('./test.txt');
-echo $f->read();
+$account1 = new BankAccount(0);
+var_dump($account1);
+$account = new BankAccount(100.5);
+var_dump($account);
+
+
+//Readonly-properties
+class UserOld
+{
+    public readonly string $username;
+
+    // public function __construct(string $username)
+    // {
+    //     $this->username = $username;
+    // }
+    public function setUserName(string $username)
+    {
+        $this->username = $username;
+    }
+}
+
+// $user = new User('Huy');
+// var_dump($user);
+$user = new UserOld();
+$user->setUserName('Huy');
+//$user->setUserName('Hi'); //error if setUserName the second time
+var_dump($user);
+
+
+//Example with User and UserProfile
+class UserProfile
+{
+    public function __construct(private string $name, private string $phone)
+    {
+    }
+
+    public function changePhoneNumber($phone) {
+        $this->phone = $phone;
+    }
+}
+
+class User
+{
+    private readonly string $username;
+    private readonly UserProfile $profile;
+
+    public function __construct(string $username)
+    {
+        $this->username = $username;
+    }
+
+    public function setProfile(UserProfile $profile)
+    {
+        $this->profile = $profile;
+    }
+
+    public function profile(): UserProfile
+    {
+        return $this->profile;
+    }
+}
+
+
+$user = new User('huytm');
+$user->setProfile(new UserProfile('Tran Minh Huy', '0823749143'));
+var_dump($user);
+$user->profile()->changePhoneNumber('0961367308');
+var_dump($user);
